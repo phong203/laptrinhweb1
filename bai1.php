915ec4abee29d8a7b99c664b1bf9f5b85fc7015c
phong203 <hoangphong2031997@gmail.com>
2018-03-14 22:14:29 +0700

    ok

---
+++ bai1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		$x = 'Chào mừng các	bạn	đến	với	môn	học	Lập	Trình Web 1';
+		echo substr($x,48)."<br>";
+
+		echo substr($x,0,5)."<br>";
+		echo substr($x,5,7)."<br>";
+		echo substr($x,13,4)."<br>";
+		echo substr($x,18,5)."<br>";
+		echo substr($x,24,6)."<br>";
+		echo substr($x,30,6)."<br>";
+	
+		
+	 ?>
+</body>
+</html>
---
+++ bai2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<body>
+       <form method='POST'> 
+       <h2>Bai Tập Định Dạng Ngày </h2>
+        <span>Nhập Ngày:</span>
+	   <input type="text" name="ten_dang_nhap"> 
+       </form>
+       
+</body>
+</html>
