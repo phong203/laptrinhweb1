a03f2917a6837aeb47003a57d858931946625a2e
phong203 <hoangphong2031997@gmail.com>
2018-03-21 23:39:40 +0700

    updata
    ok

---
+++ baitaptuan3/bai4.php
@@ -0,0 +1,50 @@
+<head>
+    <link rel="stylesheet" href="css/bootstrap.min.css"
+     
+</head>
+   <style>
+        button{
+            text-align: center;
+        }
+        </style>
+<h1> Thuc Don </h1>
+<div class="container">
+    <div class="row">
+    <div class="col-md-4">
+        <h2>Mon Khai Vi:</h2>
+        <select multiple="khaivi">
+            <option value="goi" >Goi ngo sen </option>
+             <option value="salat" >Sa lat Ca Ngu </option>
+              <option value="botron" >Bo Tron Rau Thom </option>
+               <option value="thit" >Thit Nguoi </option>
+    </select>
+    </div>
+    <div class="col-md-4">
+        <h2>Mon Chinh:</h2>
+         <select multiple="monchinh">
+            <option value="boHam" >Bo Ham </option>
+             <option value="ca" >Ca Chem Sot Ca </option>
+              <option value="tom" >Tom Rang Muoi </option>
+               <option value="cua" >Cua Sot Me </option>
+                <option value="che" >Che Hat Sen </option>
+             
+    </select>
+    </div>
+    <div class="col-md-4">
+        <h2>Mon Trang Mieng:</h2>
+          <select multiple="trangmieng">
+            <option value="HatSen" >Che Hat Sen </option>
+             <option value="flan" >Banh flan </option>
+              <option value="rauCau" >rau Cau </option>
+               
+    </div>
+</div>
+
+
+
+</div>
+<button name="button" type="submit">Submit</button>
+
+<?php
+    
+?>
